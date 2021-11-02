a468dc6d29caea33e403651fc3087208c4ab02c1
JLCardona <jcardona@cifpfbmoll.eu>
2021-11-02 23:56:14 +0100

    commit

--- app/src/main/java/com/example/task13/MainActivity.java
+++ app/src/main/java/com/example/task13/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.task13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button changeText;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.article);
+        changeText = (Button) findViewById(R.id.button);
+        changeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("NICE ARTICLE!!!");
+            }
+        });
     }
 }
